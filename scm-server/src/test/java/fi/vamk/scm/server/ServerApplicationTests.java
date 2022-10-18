7966e80271b7c0dd1ca097c1cb3331f86cad307d
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2022-10-18 15:17:44 +0300

    Server Back to MariaDB

--- scm-server/src/test/java/fi/vamk/scm/server/ServerApplicationTests.java
+++ scm-server/src/test/java/fi/vamk/scm/server/ServerApplicationTests.java
@@ -1,13 +1,9 @@
 package fi.vamk.scm.server;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }

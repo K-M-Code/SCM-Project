6647fd766b24f90bd00af8e093b7bb56c3f851c3
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2022-10-24 11:39:50 +0300

    Feature Add - Proxy

--- scm-client/src/controllers/TruckController.ts
+++ scm-client/src/controllers/TruckController.ts
@@ -1,7 +1,7 @@
 import { Truck } from '../type/Truck';
 
 export async function getTruckData(): Promise<any> {
-    const response = await fetch('http://localhost:8080/api/truck');
+    const response = await fetch('/api/truck');
     if(!response.ok) {
         throw new Error(response.statusText);
     } else {

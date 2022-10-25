6871f91c967c6cf11d677cd6e4b631f4bf2d4bce
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2022-10-25 17:34:09 +0300

    Update to Frontend

--- scm-client/src/containers/TruckContainer.tsx
+++ scm-client/src/containers/TruckContainer.tsx
@@ -1,11 +1,12 @@
 import {useState, useEffect, FC} from 'react';
 import TruckDatagridComponent from '../components/TruckDatagridComponent';
-
+import styles from './truckContainer.module.css'; 
 
 const TruckContainer:FC=props=> {
     return(
-        <div>
+        <div className='fullBody'>
             <h1>SCM Application</h1>
+            
             <TruckDatagridComponent/>
             
         </div>

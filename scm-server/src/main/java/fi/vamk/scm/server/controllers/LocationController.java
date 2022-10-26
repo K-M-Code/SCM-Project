67484b62595b29de938f4e0c10a69aab19c6cb49
KunalMenon <15151339+K-M-Code@users.noreply.github.com>
2022-10-26 14:09:27 +0300

    Location Update

---
+++ scm-server/src/main/java/fi/vamk/scm/server/controllers/LocationController.java
@@ -0,0 +1,34 @@
+package fi.vamk.scm.server.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.vamk.scm.server.entities.Location;
+import fi.vamk.scm.server.repositories.LocationRepository;
+
+@RestController
+@RequestMapping("/api/location")
+public class LocationController {
+    @Autowired LocationRepository repository;
+    
+    @GetMapping
+    public List<Location> readAll(){
+        return repository.findAll();
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Location item){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(item));
+    }
+
+
+}
+
